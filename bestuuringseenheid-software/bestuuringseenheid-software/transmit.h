0ad6482a9264e2b4cdff3e72f5c4b30399f9c43e
Bas Haaksema <b.haaksema@outlook.com>
2016-11-10 20:23:10 +0100

    Add Transmit functionality

---
+++ bestuuringseenheid-software/bestuuringseenheid-software/transmit.h
@@ -0,0 +1,16 @@
+/*
+ * tramsmit.h
+ *
+ * Created: 10-Nov-16 19:41:55
+ *  Author: Bas Haaksema
+ */ 
+
+#include <stdint.h>
+
+#ifndef UART_H_
+#define UART_H_
+
+void uart_init(void);
+void transmit(uint8_t data);
+
+#endif /* UART_H_ */
