efd1fe472dfa57baf03cc943c79d646b24d8d7f6
Unknown <glwwitteveen@live.nl>
2016-11-17 12:17:40 +0100

    Almost fix Plot

--- centrale-software/GUI.py
+++ centrale-software/GUI.py
@@ -17,6 +17,8 @@ deviceInfo = {
 connectedDevices = []
 amountOfArduinos = 0
 ser = 0
+Graph1 = 0
+Graph2 = 0
     
 def checkDevices():
     global connectedDevices
@@ -47,7 +49,6 @@ def checkDevices():
                 toggleTab(x)
                 connectedDevices.append(arduino_ports[x])
                 amountOfArduinos += 1
-                ser = serial.Serial(connectedDevices[0], 9600)
 
     tkgui.after(5000, checkDevices)
 
@@ -61,10 +62,11 @@ def toggleTab(deviceID):
     else:
         notebook.tab(deviceID, state='normal')
         
-def toggleScreen(tabName):
+def toggleScreen(tabName, portNumber):
     global ser
-    if(ser.isOpen() == False):
-        ser = serial.Serial(connectedDevices[0], 9600)
+    if(ser == 0):
+        ser = serial.Serial('COM5', 9600)
+        sleep(3)
     if (deviceInfo[tabName]['status'] == 'up'):
         ser.write(c_uint8(int('0x0b', 16))) #0b = rood lampje aan
         deviceInfo[tabName]['status'] = 'down'
@@ -73,19 +75,20 @@ def toggleScreen(tabName):
         deviceInfo[tabName]['status'] = 'up'
 
 def toggleMode(tabName, labelNumber):
-    if(DeviceInfo[tabName]['mode'] == 'manual'):
-        DeviceInfo[tabName]['mode'] = 'automatic'
-        labelNumber['text'] = 'Modus: %s' % (DeviceInfo[tabName]['mode'])
+    if(deviceInfo[tabName]['mode'] == 'manual'):
+        deviceInfo[tabName]['mode'] = 'automatic'
+        labelNumber['text'] = 'Mode: %s' % (deviceInfo[tabName]['mode'])
     else:
-        DeviceInfo[tabName]['mode'] = 'manual'
-        labelNumber['text'] = 'Modus: %s' % (DeviceInfo[tabName]['mode'])
+        deviceInfo[tabName]['mode'] = 'manual'
+        labelNumber['text'] = 'Mode: %s' % (deviceInfo[tabName]['mode'])
 
-def fillTab(frameNumber, tabName, labelNumber):
+def fillTab(frameNumber, tabName, labelNumber, portNumber):
+    global Graph1, Graph2
 
     tkToggleScreenButton = tk.Button(
         frameNumber,
         text='Toggle Screen',
-        command=lambda: toggleScreen(tabName))
+        command=lambda: toggleScreen(tabName, portNumber))
     tkToggleScreenButton.grid(column=0, row=1, sticky='W')
 
     tkToggleModeButton = tk.Button(
@@ -94,25 +97,52 @@ def fillTab(frameNumber, tabName, labelNumber):
         command=lambda: toggleMode(tabName, labelNumber))
     tkToggleModeButton.grid(column=0, row=2, sticky='W')
 
-    Graph = Plot(frameNumber, 0, 5)
-    Graph2 = Plot(frameNumber, 1, 5)
+    tkGetDataButton = tk.Button(
+        frameNumber,
+        text='Get data',
+        command = getData)
+    tkGetDataButton.grid(column=0,row=1, sticky='E')
+
+    temperatureLabel = tk.Label(frameNumber, text='Temperature: ')
+    temperatureLabel.grid(column=0, row=5, sticky='W')
+
+    brightnessLabel = tk.Label(frameNumber, text='Brightness: ')
+    brightnessLabel.grid(column=1, row=5, sticky='W')
+    
+    Graph1 = Plot(frameNumber, 0, 6)
+    Graph2 = Plot(frameNumber, 1, 6)
     
     #tkgui.after(300, Graph.step(90))
     #tkgui.root.update()
     
-    labelNumber = tk.Label(frameNumber, text='Modus: %s' % (deviceInfo[tabName]['mode']))
+    labelNumber = tk.Label(frameNumber, text='Mode: %s' % (deviceInfo[tabName]['mode']))
     labelNumber.grid(column=0, row=3, sticky='W')
 
-def gatherData():
-    pass
-    tkgui.after(2000, gatherData)
+    return Graph1, Graph2
+
+def getData():
+    global ser, Graph1, Graph2
+    if not (len(connectedDevices) == 0):
+        if(ser == 0):
+            ser = serial.Serial('COM5', 9600)
+            sleep(3)
+        
+        for x in range(2):
+            ser.write(c_uint8(int(0)))
+            s = ser.read()
+            if (s.hex() == '0a'):
+                s = ser.read()
+                print(int(s.hex(), 16))
+            elif (s.hex() == '0b'):
+                s = ser.read()
+                print(int(s.hex(), 16))
 
 #Make a window 
 tkgui = tk.Tk()
 #set window size
 tkgui.geometry('600x500')
 #set window title
-tkgui.title('Besturingscentrale')
+tkgui.title('Controlsystem')
 
 #make label
 tkLabelTop = tk.Label(tkgui, text='')
@@ -138,17 +168,17 @@ notebook.add(frame4, text='Tab 4', sticky='EW', state='disabled')
 notebook.add(frame5, text='Tab 5', sticky='EW', state='disabled')
 notebook.pack()
 
-fillTab(frame1, 'tab1', 'label1')
-fillTab(frame2, 'tab2', 'label2')
-fillTab(frame3, 'tab3', 'label3')
-fillTab(frame4, 'tab4', 'label4')
-fillTab(frame5, 'tab5', 'label5')
+fillTab(frame1, 'tab1', 'label1', 0)
+fillTab(frame2, 'tab2', 'label2', 1)
+fillTab(frame3, 'tab3', 'label3', 2)
+fillTab(frame4, 'tab4', 'label4', 3)
+fillTab(frame5, 'tab5', 'label5', 4)
 
-tkButtonCheckDevices = tk.Button(
-    tkgui,
-    text='Check for devices',
-    command=checkDevices)
-tkButtonCheckDevices.pack()
+#tkButtonCheckDevices = tk.Button(
+#    tkgui,
+#    text='Check for devices',
+#    command=checkDevices)
+#tkButtonCheckDevices.pack()
 
 tkButtonQuit = tk.Button(
     tkgui,
@@ -158,5 +188,4 @@ tkButtonQuit.pack()
 
 checkDevices()
 tkgui.after(3000, checkDevices)
-#tkgui.after(2000, gatherData)
 tkgui.mainloop()
